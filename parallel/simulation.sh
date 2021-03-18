5056ec88db167d595d1c6f0f036303f81fa28d75
ascordeiro <alinesantanacordeiro@gmail.com>
2021-03-18 13:04:20 +0000

    minor fixings

--- parallel/simulation.sh
+++ parallel/simulation.sh
@@ -14,22 +14,21 @@ do
             CONFIG_FILE="configuration_files/sandy_bridge_${THREADS}cores.cfg"
             for inst in 4096 #8192 16384 32768 65536 131072 262144
             do
-                for feat in 4096 8192 #8 16 32 64 128 256 512 1024 2048 
+                for feat in 4096 8192 #8 16 32 64 128 256 512 1024 2048
                 do
                     for i in mlp_avx_${inst}i_${feat}f_${THREADS}t.tid0.stat.out.gz
-                    do 
+                    do
                         cd $SIM_HOME
                         TRACE=mlp_avx_${inst}i_${feat}f_${THREADS}t
                         COUNTER=0
                         COMMAND="./orcs"
-                    
                         while [ $COUNTER -lt $THREADS ]; do
                             COMMAND=${COMMAND}' -t '${TRACE_HOME}/${TRACE}
                             let COUNTER=COUNTER+1
                         done
                         nohup ${COMMAND} -c ${CONFIG_FILE} &> ${CODE_HOME}/results/${TRACE}.txt &
                     done
-                done 
+                done
             done
         ;;
         knn|KNN|kNN)
@@ -42,12 +41,11 @@ do
                     for k in 9 #5 13
                     do
                         for i in knn_avx_${inst}i_${feat}f_${k}k_${THREADS}t.tid0.stat.out.gz
-                        do 
+                        do
                             cd $SIM_HOME
                             TRACE=knn_avx_${inst}i_${feat}f_${k}k_${THREADS}t
                             COUNTER=0
                             COMMAND="./orcs"
-                        
                             while [ $COUNTER -lt $THREADS ]; do
                                 COMMAND=${COMMAND}' -t '${TRACE_HOME}/${TRACE}
                                 let COUNTER=COUNTER+1
